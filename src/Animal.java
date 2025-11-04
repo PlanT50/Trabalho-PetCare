3a3bba1ae1a510c8035fb36e015ced37f11f63f0
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-04 14:19:02 -0300

    Adiconado o construtor e os getters e setters

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,32 @@
 public class Animal{
     String especie;
     int idade;
-    String Histórico;
+    String historico;
+    String proprietario;
+
+    public Animal(String especie, int idade, String historico, String proprietario){
+        this.especie = especie;
+        this.idade = idade;
+        this.historico = historico;
+        this.proprietario = proprietario;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getHistorico(){
+        return historico;
+    }
+    public void setHistorico(String historico){
+        this.historico = historico;
+    }
+    public String getProprietario() {
+        return proprietario;
+    }
+
 }
