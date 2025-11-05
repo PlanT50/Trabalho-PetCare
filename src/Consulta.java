13a5bb07cdb73cecbac10301860e66eab2b47d73
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-05 12:26:19 -0300

    Atributo hora alterado para String e método cancelar cumprido

--- src/Consulta.java
+++ src/Consulta.java
@@ -1,24 +1,25 @@
 public class Consulta implements Agendavel {
     private String data;
-    private int hora;
+    private String hora;
     private Proprietario proprietario;
     private Animal animal;
 
-    public Consulta(String data, int hora, Proprietario proprietario, Animal animal) {
+    public Consulta(String data, String hora, Proprietario proprietario, Animal animal) {
         this.data = data;
         this.hora = hora;
         this.proprietario = proprietario;
         this.animal = animal;
     }
     @Override
-    public void agendar(String data, int hora){
+    public void agendar(String data, String hora){
         this.data = data;
         this.hora = hora;
         System.out.println("Consulta agendada para dia" + data + "ás " + hora);
     }
     @Override
     public void cancelar(){
-
+        setData(null);
+        setHora(null);
     }
     public String getData(){
         return data;
@@ -26,10 +27,10 @@ public class Consulta implements Agendavel {
     public void setData(String data){
         this.data = data;
     }
-    public int getHora(){
+    public String getHora(){
         return hora;
     }
-    public void setHora(int hora){
+    public void setHora(String hora){
         this.hora = hora;
     }
 }
