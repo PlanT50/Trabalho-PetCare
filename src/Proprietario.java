0d287de09f7654d237713d84fb23474d02847620
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-05 09:17:52 -0300

    Array list de animais adicionada na classe proprietário, junto com os getters e setters dos outros atributos

--- src/Proprietario.java
+++ src/Proprietario.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+
 public class Proprietario {
     private int contato;
     private String assinatura;
     private boolean Vip;
+    private ArrayList<Animal> animais;
+
+    public Proprietario(int contato, String assinatura, boolean Vip) {
+        this.contato = contato;
+        this.assinatura = assinatura;
+        this.Vip = Vip;
+        animais = new ArrayList<>();
+    }
+
+    public int getcontato(){
+        return contato;
+    }
+
+    public void setcontato(int contato){
+        this.contato = contato;
+    }
+
+    public String getassinatura(){
+        return assinatura;
+    }
+
+    public void setassinatura(String assinatura){
+        this.assinatura = assinatura;
+    }
+
+    public boolean getVip(){
+        return Vip;
+    }
+
+    public void setVip(boolean Vip){
+        this.Vip = Vip;
+    }
 
 
 }
