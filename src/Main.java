d6e95bd547f8285f95d5a313c83175fe7ee4aa91
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-10 23:43:26 -0300

    Remoçãó de duas classes

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Proprietario p1 = new Proprietario("Kevin",988135943,"Premium",false);
-        Animal a1 = new Animal("Lua","Gato",2,"Saudável",p1);
-        Consulta c1 = new Consulta("05/11","10:30",a1);
-        c1.exibirConsulta(a1);
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
 
 
 
