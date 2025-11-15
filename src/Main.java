97ced5a7c8a339c37ff143d1d714fe96677be976
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-14 22:53:05 -0300

    Parte do menu adicionado

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TelaPrincipal telaPrincipal = new TelaPrincipal();
-        telaPrincipal.setVisible(true);
+
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
 
 
+        while (opcao != 5) {
+            System.out.println("========== PET CARE ==========");
+            System.out.println("1 - Cadastrar Proprietário");
+            System.out.println("2 - Cadastrar Animal");
+            System.out.println("3 - Agendar Consultas");
+            System.out.println("4 - Verificar Consultas");
+            System.out.println("5 - Sair");
 
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+            }
+
+
+        }
     }
 }
